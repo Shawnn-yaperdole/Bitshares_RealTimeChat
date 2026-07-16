49828705f21e269a29bc2a495ba7773c8eceb4e5
Shawn-Sabela <Shawns.chocolat@gmail.com>
2026-07-17 02:59:35 +0800

    profile picture feature implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+
 
 dotenv.config();
 
@@ -13,14 +15,15 @@ const __dirname = path.resolve();
 
 const PORT = ENV.PORT;
 
-app.use(express.json()) //req.body
+app.use(express.json()); //req.body
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 //deployment prep
 if(ENV.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("*", (req,res) => {
         res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
