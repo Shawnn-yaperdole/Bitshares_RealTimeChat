776679b84e4855b8fb84c570405eab375636ebdb
Shawn-Sabela <Shawns.chocolat@gmail.com>
2026-07-17 03:02:26 +0800

    refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -106,7 +106,7 @@ export const updateProfile = async (req, res) => {
         const updatedUser = await User.findByIdAndUpdate(
             userId, 
             { profilePic: uploadResponse.secure_url }, 
-            {new:true}
+            { new: true }
         );
 
         res.status(200).json(updatedUser);
